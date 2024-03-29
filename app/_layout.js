b6bb9db12332bcdcb76be8072eb44412e8956713
Krish Maha <krish@mahas.net>
2024-03-29 11:50:34 +0000

    Add welcome page components

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,30 @@
 import { Stack } from 'expo-router'
+import { useCallback } from 'react'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+// Makes splash screen visible when app is loading
+// SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf')
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      // Only show home page if fonts have been loaded
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+  return <Stack onLayout={onLayoutRootView}/>
+  // return <Stack />
 }
 
 export default Layout; 
