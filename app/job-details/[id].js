c64bf53b3f2e6f1ca0dbe7d0122d6b929aac3b3f
Krish Maha <krish@mahas.net>
2024-04-01 00:30:43 +0100

    Retrieve points for qualifications tab for job

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -6,6 +6,8 @@ import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } fro
 import { COLORS, icons, SIZES } from '../../constants'
 import useFetch from '../../hooks/useFetch.js'
 
+const tabs = ["About", "Qualifications", "Responsibilities"];
+
 const JobDetails = () => {
   // get specific ID of job details page we are on
   const params = useLocalSearchParams();
@@ -16,8 +18,25 @@ const JobDetails = () => {
   })
 
   const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
 
   const onRefresh = () => {}
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "About":
+        
+        break;
+      case "Qualifications":
+        return <Specifics
+          title="Qualifications"
+          points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+        />
+          break;
+      case "Responsibilities":
+            
+            break;  
+    }
+  }
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
@@ -62,8 +81,11 @@ const JobDetails = () => {
 
               />
               <JobTabs 
-
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
               />
+              {displayTabContent()}
             </View>
           )}
         </ScrollView>
